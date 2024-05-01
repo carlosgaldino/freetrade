f0a27031205f6e871c5f77ce69a56b25c1c9f874
Carlos Galdino <carloshsgaldino@gmail.com>
2024-05-01 08:02:00 +0100

    Return errors directly

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args().skip(1);
-    let mut input_file = String::new();
-    let mut account_name = String::new();
+    let input_file;
+    let account_name;
     match args.next() {
-        None => exit(1),
+        None => return Err(USAGE.into()),
         Some(s) => match s.as_str() {
-            "-h" | "--help" => exit(0),
+            "-h" | "--help" => {
+                eprintln!("{}", USAGE);
+                return Ok(());
+            }
             "-i" | "--input" => {
-                let Some(file)= args.next() else {
-                    return exit(1);
-                };
-                input_file = file;
-                match expect_arg("-a", "--account", &mut args) {
-                    Some(account) => account_name = account,
-                    None => exit(1),
-                }
+                input_file = args.next().ok_or(USAGE)?;
+                account_name = expect_arg("-a", "--account", &mut args)?;
             }
             "-a" | "--account" => {
-                let Some(account)= args.next() else {
-                    return exit(1);
-                };
-                account_name = account;
-                match expect_arg("-i", "--input", &mut args) {
-                    Some(file) => input_file = file,
-                    None => exit(1),
-                }
+                account_name = args.next().ok_or(USAGE)?;
+                input_file = expect_arg("-i", "--input", &mut args)?;
             }
-            _ => exit(1),
+            _ => return Err(USAGE.into()),
         },
     }
     println!("input: {}, account: {}", input_file, account_name);
@@ -36,18 +27,14 @@ fn expect_arg(
     arg_short: &str,
     arg_long: &str,
     args: &mut impl Iterator<Item = String>,
-) -> Option<String> {
-    let arg = args.next()?;
-    match arg.as_str() {
+) -> Result<String, Box<dyn std::error::Error>> {
+    let arg = args.next().ok_or(USAGE)?;
+    let arg = match arg.as_str() {
         short if short == arg_short => args.next(),
         long if long == arg_long => args.next(),
-        _ => None,
-    }
-}
-
-fn exit(code: i32) {
-    eprintln!("{}", USAGE);
-    std::process::exit(code);
+        _ => return Err(USAGE.into()),
+    };
+    arg.ok_or_else(|| USAGE.into())
 }
 
 const USAGE: &str = r#"freetrade - A beancount importer for Freetrade
